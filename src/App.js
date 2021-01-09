793c3710f55af41b6decec55e633fb2486bd1694
ACaspersz <a.casperszz@gmail.com>
2021-01-09 21:02:43 +1100

    initial

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import { Home } from './Home';
+import { Search } from './Search';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { LoginCallback, SecureRoute, Security } from '@okta/okta-react';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <header>
+          <div>Books with Hooks</div>
+          <ul className="menu"><li><Link to="/">Home</Link></li><li><Link to="/search">Search</Link></li></ul>
+        </header>
+        <Security issuer='https://dev-6853890.okta.com/oauth2/default'
+                  clientId='0oa3m9s7mQdUqDRGy5d6'
+                  redirectUri={window.location.origin + '/callback'}
+                  pkce={true}>
+          <Route path='/' exact={true} component={Home}/>
+          <SecureRoute path='/search' exact={true} component={Search}/>
+          <Route path='/callback' component={LoginCallback}/>
+        </Security>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
